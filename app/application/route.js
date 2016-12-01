cd5f29549c0362581689ee0d0943783ace3ee5c3
Markham Shofner <markham.shofner@gmail.com>
2016-12-01 14:29:21 -0500

    enable internationalization

--- app/application/route.js
+++ app/application/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  intl: Ember.inject.service(),
+
+  beforeModel: function () {
+    // set base language to english, will need TODO build out alternative options
+    const intl = this.get('intl');
+    let defaultLocale = 'en-us';
+    intl.setLocale(defaultLocale);
+    // let translationKey = this._calculateTranslationKey(defaultLocale);
+  }
 });
