0d3b31b466f9daca882ecae49124b11a585b8e57
Markham Shofner <markham.shofner@gmail.com>
2017-01-09 13:33:40 -0500

    data citation upgrade to show opendata dataset links and  agol item links, as well as the existing server link

--- app/components/data-citation/component.js
+++ app/components/data-citation/component.js
@@ -1,5 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // console.log(this.get('featureService'));
+  ajax: Ember.inject.service(),
+
+  init () {
+    this._super.apply(this, arguments);
+  },
+
+  didReceiveAttrs() {
+    let urlRegex = this.get('layer.url').replace(/.*?\/\//g,"http://");
+    let urlFilter = `https://opendata.arcgis.com/api/v2/datasets?include=sites&filter[url]=${urlRegex}`
+    console.log('urlFilter', urlFilter);
+    this.get('ajax').request(urlFilter, {dataType: 'json'})
+      .then((response) =>{
+        console.log(response);
+        this.set('datasetName', response.data[0].attributes.name);
+        this.set('attributeName', response.included[0].attributes.title);
+
+        let baseUrl = response.included[0].attributes.url;
+        let id = response.data[0].id;
+        let odLink = `${baseUrl}/datasets/${id}`;
+
+        this.set('odLink', odLink);
+        this.set('agolLink', response.data[0].attributes.landingPage);
+        this.set('serverLink', this.get('layer.url'));
+
+      });
+  }
+
 });
