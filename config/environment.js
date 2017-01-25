574b8495937e9c37fa4f8f3d12f2147d174a5365
Markham Shofner <markham.shofner@gmail.com>
2017-01-25 11:42:41 -0500

    tiny changes II for gh pages

--- config/environment.js
+++ config/environment.js
@@ -75,11 +75,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // ENV.locationType = 'hash';
+    ENV.locationType = 'hash';
     ENV.rootURL = '/open-streets/';
-    // brought in from torii
-    // ENV.locationType = 'hash';
-    // ENV.rootURL = '/your-repo-name/';
     ENV.APP.baseURL = 'opendata.arcgis.com';
   }
 
