2d98a3fcda000903c314d8b506bcbbc27498a82d
Markham Shofner <markham.shofner@gmail.com>
2017-01-26 22:28:21 -0500

    move logic from application route to index route

--- app/application/route.js
+++ app/application/route.js
@@ -1,38 +1,6 @@
 import Ember from 'ember';
 import ENV from '../config/environment';
 
-// TODO: move this to util function and unit test
-function processConfigParams (params) {
-  // TODO the below variables are taken from financial-transparency app, and not applicable here, but
-    // the pattern for converting / parsing may be useful
-  // parse comma seperated value string for aspect fields
-  if (typeof params.aspectFields === 'string') {
-    const aspectFields = params.aspectFields.split(',').map(aspectField => {
-      const trimmedAspectField = aspectField.trim();
-      if (trimmedAspectField !== '') {
-        return trimmedAspectField;
-      }
-    });
-    if (aspectFields.length > 0) {
-      params.aspectFields = aspectFields;
-    } else {
-      delete params.aspectFields;
-    }
-  }
-  // convert subField to an array if subFields is missing
-  if (!params.subFields && params.subField) {
-    params.subFields = [params.subField];
-    delete params.subField;
-  }
-
-  // TODO - verify that passed in - Geocode Locator - geocodeUrl is actually a url
-    // (then in model function set as .APP.geocodeUrl)
-
-  // establish that themeId is a guid (globally unique id) - isGuid function
-
-  return params;
-}
-
 export default Ember.Route.extend({
   intl: Ember.inject.service(),
   session: Ember.inject.service(),
@@ -55,41 +23,8 @@ export default Ember.Route.extend({
   },
 
   model (params) {
-    // read params from environment config
-    const {
-      geocodeUrl,
-      webappId,
-      themeId,
-      testString,
-      testInt
-    } = ENV.APP;
-    const config = {
-      geocodeUrl,
-      webappId,
-      themeId,
-      testString,
-      testInt
-    };
-
-    // TODO: also validate appid is a guid
     if (params.appid) {
-      // get config params from an AGO item
-      return this.get('itemService').getDataById(params.appid)
-      .then((response) => {
-        const params = processConfigParams(response.values);
-        // mixin any missing params from enviornment config and return
-        this.set('appSettings.config', Object.assign(config, params));
-        this.transitionTo(`/${this.get('appSettings.config.webappId')}`);
-      }, (err) => {
-        // error fetching config from AGO item
-        const error = err.message || err;
-        return { error };
-      });
-    } else {
-      // just use environment config
-      this.set('appSettings.config', config);
-      this.transitionTo('examples');
+      this.transitionTo(`/${params.appid}`);
     }
   }
-
 });
--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
+
+// TODO: move this to util function and unit test
+function processConfigParams (params) {
+  // TODO the below variables are taken from financial-transparency app, and not applicable here, but
+    // the pattern for converting / parsing may be useful
+  // TODO - verify that passed in - Geocode Locator - geocodeUrl is actually a url
+    // (then in model function set as .APP.geocodeUrl)
+
+  // establish that themeId is a guid (globally unique id) - isGuid function
+
+  return params;
+}
 
 export default Ember.Route.extend({
   itemsService: Ember.inject.service(),
@@ -30,23 +43,44 @@ export default Ember.Route.extend({
     // TODO Eventually, check if params.id === null, then make a
     // this.get('itemService').search(...) call to locate all the App Config Items for Open Street.
 
+    // read params from environment config
+    const {
+      geocodeUrl,
+      webappId,
+      themeId,
+      webmap
+    } = ENV.APP;
+    const config = {
+      geocodeUrl,
+      webappId,
+      themeId,
+      webmap
+    };
+
+    // TODO: also validate appid is a guid
+
     return Ember.RSVP.hash({
       item: this.get('itemsService').getById(params.id),
       data: this.get('itemsService').getDataById(params.id),
     })
     .then((results) => {
-      console.log('results from index route item call:', results);
+      const params = processConfigParams(results.values);
+      // mixin any missing params from enviornment config and return
+
       this.get('appSettings').set('settings', results);
-      // this.get('appSettings').set('errStatus', 200);
+
+      this.set('appSettings.config', Object.assign(config, params));
+
+      console.log('appSettings', this.get('appSettings'));
+      console.log('appSettings.settings', this.get('appSettings.settings'));
       return this.get('itemsService').getDataById(results.data.values.webmap)
     })
     .then((webmap) => {
-      // store in appSettings
-      console.log('webmap from index route item call:', webmap);
       this.get('appSettings').set('settings.webmap', webmap);
     })
     .catch((err) => {
       this.get('appSettings').set('errStatus', err.code || 500);
+      this.transitionTo('examples');
       Ember.debug('Error occured fetching the item: ' + JSON.stringify(err));
     });
   },
