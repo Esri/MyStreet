bd3f8bc06bb645890c8b0e12ad939b383565c267
Tom Wayson <tom@tomwayson.com>
2017-05-29 07:38:42 -0700

    use JSAPI 3.x b/c 4.x can't load web map JSON

--- app/application/route.js
+++ app/application/route.js
@@ -26,8 +26,9 @@ export default Ember.Route.extend({
     // render the template as normal
     this._super(...arguments);
     // then preload the JSAPI
-    // NOTE: to use the latest 4.x release don't pass any arguments to load()
-    this.get('esriLoader').load().catch(err => {
+    this.get('esriLoader').load({
+      url: 'https://js.arcgis.com/3.20'
+    }).catch(err => {
       // do something with the error
       Ember.debug(`application:route:renderTemplate:esriLoader error: ${err}`);
     });
