be77b797f65176400cd58efb1d55c415c2c57a2c
Markham Shofner <markham.shofner@gmail.com>
2017-10-26 12:23:18 -0400

    bump esri loader version

--- config/environment.js
+++ config/environment.js
@@ -72,7 +72,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/mystreet/';
+    ENV.rootURL = '/mystreet/'; //'/mystreet/';
     ENV.APP.baseURL = 'opendata.arcgis.com';
   }
 
